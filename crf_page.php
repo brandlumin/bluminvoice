8b0960a959b5e9aee9164b70e67ec15387bdae0e
brandlumin <development+newsletter@brandlumin.com>
2021-06-03 17:19:44 +0530

    v0.1.10 =======
    Unnecessary fields removed.

--- crf_page.php
+++ crf_page.php
@@ -1,8 +1,8 @@
 <?php
-  $fCrfCust = $_POST["form-crf-cust"]; $fCrfCustID = $_POST["form-crf-custID"]; $fCrfProjectID = $_POST["form-crf-projectID"]; $fCrfProject = $_POST["form-crf-project"]; $fCrfDate = $_POST["form-crf-date"]; $fCrfDesc = $_POST["form-crf-desc"]; $fCrfAmount = $_POST["form-crf-amount"];
+  $fCrfCust = $_POST["form-crf-cust"]; $fCrfProjectID = $_POST["form-crf-projectID"]; $fCrfProject = $_POST["form-crf-project"]; $fCrfDate = $_POST["form-crf-date"]; $fCrfDesc = $_POST["form-crf-desc"]; $fCrfAmount = $_POST["form-crf-amount"];
 
   /* setting the query */
-  $query = "INSERT INTO `changeMaster`(`custID`, `prjID`, `changeReq`, `changeDate`, `changeAmount`) VALUES ('$fCrfCustID' ,'$fCrfProjectID' ,'$fCrfDesc' ,'$fCrfDate' ,'$fCrfAmount')";
+  $query = "INSERT INTO changeMaster(prjID, changeReq, changeDate, changeAmount) VALUES ('$fCrfProjectID' ,'$fCrfDesc' ,'$fCrfDate' ,'$fCrfAmount')";
 
   /* establishing connection */
   @include "consql.php";
